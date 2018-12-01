fe2412eeeaaa8c4bd6e74a037423a45b35d35828
Eirik Vestgren <eirik.vestgren@outlook.com>
2018-12-01 17:48:35 +0100

    Update heath-jarrow-morton_implementation.R

--- heath-jarrow-morton_implementation.R
+++ heath-jarrow-morton_implementation.R
@@ -8,8 +8,8 @@
 
 #Should more things be returned? If yes, then what? 
 
-
 source('utility_functions.R')
+
 
 #Inputs to HJM drift: 
 #S_j(k), j = 1...M-i, k = 1...d, the volatility of the forward rates. 
@@ -19,16 +19,28 @@ HJM_drift = function(s_df, maturities, nFactors)
   d = nFactors 
   s_df = as.matrix(s_df)
   M = length(maturities) 
+  
+  
+  
+  if (maturities[1] == 0)
+    {intervals = diff(maturities)}
+  else
+    {intervals = diff(c(0, maturities)); M = M+1}
+  
   if (dim(s_df)[2] != d)
   {
     s_df = t(s_df)
   }
   n_times = dim(s_df)[1]
+  
+  
   #Find out what the correct is for i, which i have called n_times
   #if(is.null(n_times))
   #{
   #  n_times = length(s_df)/d
   #}
+  
+  
   B_prev = 0;
   B_next = 0;
   final_j = n_times #dim(s_df)[1] = M-i, i.e. the final j
@@ -61,18 +73,17 @@ HJM_func = function(forward_yield_curve, times_of_yields, forward_interest_data,
   
   n_termins = length(times_of_yields)
   #Constructing the discount intervals.  
-  times_of_yields = c(0, times_of_yields)
-  diff_times_of_yields = diff(times_of_yields)
+  diff_times_of_yields = diff(c(0,times_of_yields))
   
   
-  M = length(forward_yield_curve)
+  M = length(forward_yield_curve) # Number of different times with rate data. 
   
   discount_factor = 1; 
   ZCB = 0; 
   C = 0; 
   
   #Cash flows for zero coupon bond and cap
-  
+  #Automate this 
   P_ZCB = c(rep(0, n_termins-1), 1) #Vector with cashflow at each maturity 
   P = rep(0, n_termins)
   
@@ -92,7 +103,7 @@ HJM_func = function(forward_yield_curve, times_of_yields, forward_interest_data,
     #Now, find the s_j(k)'s. This is done through Principal Component Analysis when we want to fit it to real data. 
     subset_s = all_sjks[1:(M-i), ]
     
-    m = HJM_drift(subset_s, diff_times_of_yields, nFactors)
+    m = HJM_drift(subset_s,diff_times_of_yields, nFactors)
     Z = rnorm(nFactors)
     for(j in 1:(M-i) )
     {
@@ -112,10 +123,10 @@ HJM_func = function(forward_yield_curve, times_of_yields, forward_interest_data,
   
   
   #Return ZCB if this is desired
-  return(ZCB)
+  #return(ZCB)
   #Want to return ZCB, C and the realization of the spot rate. 
   #return(data.frame(ZCB, C, )
-  #return(C)
+  return(c(C, ZCB))
 }
 
 s_eval = function(data, nFactors)
@@ -128,20 +139,9 @@ s_eval = function(data, nFactors)
   return(s)
 }
 
+
 zero_bond_cash_flow = function(times)
 {
   return(c(rep(0, length(times) -1 ), 1))
   #return(c(0,0,0,0,1))
-  #Leads to zero valuation 
-}
-zero_bond_cash_flow_early_mat = function(times)
-{
-  return(c(0,0,0,1,0))
-}
-cap_representation_cash_flow = function(final_maturity, cap_strike)
-  #How is it done when caplet is paid between maturity dates? Assume for now that they aren't 
-  #Assume payment every 6 months
-  #Which rate is should be used for the payment? Spot rate at the given time. 
-{
-  return()
 }
